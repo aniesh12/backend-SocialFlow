7a1f0b4e58ccc89cef11a29dc0570b1894138097
aniesh12 <aniesh2k3@gmail.com>
2026-02-19 16:40:07 +0530

    fix bug

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ connectDB();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL ,
   credentials: true
 }));
 
