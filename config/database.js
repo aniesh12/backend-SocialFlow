2abcca4fe0d1e40cde78b3d8699140cc9ee726d2
aniesh12 <aniesh2k3@gmail.com>
2026-02-19 17:18:52 +0530

    added bd link in web

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  console.log("Mongo URI:", process.env.MONGODB_URI);
   try {
-  const conn = await mongoose.connect(process.env.MONGODB_URI )
-  .then(() => console.log("DB Connected"))
-  .catch(err => console.error("DB Error:", err));
+    const conn = await mongoose.connect(process.env.MONGODB_URI , {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     
